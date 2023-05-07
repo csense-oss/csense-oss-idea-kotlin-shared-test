6491c1d703f39b2a32ebf9707d8ac52d7fea19a9
Kasper T <kasper@webmasteren.eu>
2023-05-07 17:06:02 +0200

    bump + started on more templates

---
+++ src/main/kotlin/csense/idea/kotlin/test/BaseLineMarkerTestFixture.kt
@@ -0,0 +1,52 @@
+package csense.idea.kotlin.test
+
+import com.intellij.codeInsight.daemon.*
+import com.intellij.lang.*
+import com.intellij.testFramework.*
+import com.intellij.testFramework.fixtures.*
+import org.junit.*
+import org.junit.runner.*
+import org.junit.runners.*
+
+
+@RunWith(JUnit4::class)
+abstract class BaseLineMarkerTestFixture : BasePlatformTestCase() {
+    private val language: Language by lazy {
+        Language.findLanguageByID("kotlin")!!
+    }
+
+    abstract val providerToTest: LineMarkerProvider
+    private var counter = 0
+
+    @Before
+    fun setup() {
+        myFixture.allowTreeAccessForAllFiles()
+        cleanAndSetProvider(language, providerToTest)
+        counter += 1
+    }
+
+    fun cleanAndSetProvider(language: Language, provider: LineMarkerProvider) {
+        LineMarkerProviders.getInstance().clearCache(language)
+        LineMarkerProviders.getInstance().addExplicitExtension(language, provider)
+    }
+
+    fun testSingleGutter(
+        code: String,
+        expectedTooltipText: String
+    ) {
+        myFixture.configureByText(
+            /* fileName = */ "test$counter.kt",
+            /* text = */ code
+        )
+        myFixture.doHighlighting()
+        val gutters: MutableList<GutterMark> = myFixture.findAllGutters()
+        gutters.assertSingle { it: GutterMark ->
+            it.tooltipText.assert(expectedTooltipText)
+        }
+    }
+
+
+    override fun getProjectDescriptor(): LightProjectDescriptor {
+        return KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE_FULL_JDK
+    }
+}
